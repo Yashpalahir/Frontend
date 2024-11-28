feb997d010bc8f11fefb394326f651aa3b1faeeb
Yashpal <yashbaldaniya26@gmail.com>
2024-11-28 23:38:32 +0530

    first commit

--- src/components/Kanban.jsx
+++ src/components/Kanban.jsx
@@ -11,13 +11,6 @@ function Kanban() {
     const [ordering, setOrdering] = useState(localStorage.getItem('ordering') || 'title');
 
 
-    // useEffect(() => {
-    //     const group = localStorage.getItem('grouping')
-    //     const order = localStorage.getItem('ordering')
-    //     if(order) {setOrdering(order)};
-    //     if(group) {setGrouping(order)};
-    // }, [])
-
     useEffect(() => {
         localStorage.setItem('ordering', ordering)
     }, [ordering])
@@ -45,4 +38,4 @@ function Kanban() {
   )
 }
 
-export default Kanban
+export default Kanban;
